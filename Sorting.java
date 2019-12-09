b3347e2de9788495beffc697cccbc4309af758b3
JayanthM <jayanth.madupalli@gmail.com>
2019-12-09 08:25:01 +0530

    Added Shell sort & Shuffling

--- Sorting.java
+++ Sorting.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Sorting{
 
     private static boolean less(Comparable a, Comparable b){
@@ -40,13 +41,35 @@ public class Sorting{
     public static void insertion_sort(Comparable[] arr){
         int n = arr.length;
         for(int i = 1; i < n; i++)
-            for(int j = i; j > 0; j--)
-                if(less(arr[j], arr[j-1]))
+            for(int j = i; j > 0 && less(arr[j], arr[j-1]); j--)
                     swap(arr, j, j-1);
-                else 
-                    break;
     }
 
+    public static void shell_sort(Comparable[] arr){
+        int n = arr.length;
+        int h = 1;
+        while(h < n/3)
+            h = 3 * h + 1; // 3x+1 increments
+        
+        while(h >= 1){
+
+            for(int i = h; i < n; i++)
+                for(int j = i; j >= h && less(arr[j], arr[j-h]); j -= h)
+                    swap(arr, j, j-h);
+            
+            h /= 3;
+        }
+    }
+
+    public static void knuth_shuffle(Comparable[] arr){
+
+        int n = arr.length;
+        Random r = new Random();
+
+        for(int i = 1; i < n; i++)
+            swap(arr, i, r.nextInt(i+1));
+
+    }
 
     public static void main(String args[]){
        
