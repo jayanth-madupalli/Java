b02d54094754e3e00ae22897737a8e78ec2afed1
JayanthM <jayanth.madupalli@gmail.com>
2019-12-09 08:31:45 +0530

    Added Merge Sort

--- Sorting.java
+++ Sorting.java
@@ -71,6 +71,33 @@ public class Sorting{
 
     }
 
+    static class MergeSort{
+        static void merge(Comparable[] a, Comparable aux[], int lo, int mid, int hi){
+            int i = lo, j = mid + 1;
+
+            for(int k = lo; k <= hi; k++)
+                aux[k] = a[k];
+
+            for(int k = lo; k <= hi; k++){
+                if(i > mid)     a[k] = aux[j++];
+                else if (j > hi)    a[k] = aux[i++];
+                else if(less(aux[j], aux[i]))      a[k] = aux[j++];
+                else    a[k] = aux[i++];
+            }
+        }
+        static void sort(Comparable[] a, Comparable[] aux, int lo, int hi){
+            if(hi <= lo)    return;
+            int mid = lo + (hi-lo) / 2;
+            sort(a, aux, lo, mid);
+            sort(a, aux, mid+1, hi);
+            merge(a, aux, lo, mid, hi);
+        }
+        static void sort(Comparable[] a){
+            Comparable aux[] = new Comparable[a.length];
+            sort(a, aux, 0, a.length-1);
+        }
+    }
+    
     public static void main(String args[]){
        
     }
